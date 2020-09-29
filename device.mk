3dab891475be44a13caf45bde21afc8771789c57
Ali Hasan <ahb7671@gmail.com>
2020-09-29 08:49:17 +0500

    mido: import DeviceParts
    https://github.com/omnirom/android_device_xiaomi_mido/tree/android-10/DeviceParts
    
    Dirac only
    
    Signed-off-by: Ali Hasan <ahb7671@gmail.com>

--- device.mk
+++ device.mk
@@ -167,6 +167,10 @@ PRODUCT_PACKAGES += \
     libgenlock \
     libtinyxml
 
+# Device-specific settings
+PRODUCT_PACKAGES += \
+	DeviceParts
+
 # DRM
 PRODUCT_PACKAGES += \
     android.hardware.drm@1.0-impl \
