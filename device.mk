fafdc0714d6bd495a123a4c3ee61cc085ad62a09
Ali Hasan <ahb7671@gmail.com>
2020-10-28 12:42:29 +0500

    mido: add prebuilt protobuf from vndk29
    Signed-off-by: Ali Hasan <ahb7671@gmail.com>

--- device.mk
+++ device.mk
@@ -297,12 +297,6 @@ PRODUCT_COPY_FILES += \
     $(LOCAL_PATH)/configs/powerhint.xml:$(TARGET_COPY_OUT_VENDOR)/etc/powerhint.xml \
     $(LOCAL_PATH)/configs/perf/perf-profile0.conf:$(TARGET_COPY_OUT_VENDOR)/etc/perf/perf-profile0.conf
 
-# Protobuf
-PRODUCT_PACKAGES += \
-    libprotobuf-cpp-full-vendorcompat \
-    libprotobuf-cpp-lite-vendorcompat
-
-
 # QMI
 PRODUCT_PACKAGES += \
     libjson
